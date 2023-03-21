264a5c130e19091ecd035eabc8e955af7799e07d
Ville Skyttä <ville.skytta@iki.fi>
2023-03-21 13:29:33 +0200

    ci: add expected modification test

--- .github/workflows/test-canary.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-
-# This script is fed to the hooks defined in this repo in CI for some
-# basic smoke tests. It must not provoke any formatting changes or
-# errors from shfmt.
-
-echo "Hello, test."
