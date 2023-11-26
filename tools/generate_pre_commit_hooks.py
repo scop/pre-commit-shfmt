66d4f7b2a5751f9288cf1de158d8a3fae4d1a19d
Ville Skyttä <ville.skytta@iki.fi>
2023-11-26 17:13:53 +0200

    refactor: switch to setuptools-download
    Plays better along with pre-commit.ci than wrun.
    
    Closes https://github.com/scop/pre-commit-shfmt/issues/20

---
+++ tools/generate_pre_commit_hooks.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+import contextlib
+import string
+import sys
+
+import docker
+
+
+hooks_template = """
+- id: shfmt
+  name: shfmt
+  description: Shell source code formatter (prebuilt upstream executable)
+  language: python
+  entry: shfmt
+  args: [-w, -s]
+  types: [shell]
+  exclude_types: [csh, tcsh, zsh]
+  stages: [commit, merge-commit, push, manual]
+
+- id: shfmt-src
+  name: shfmt
+  description: Shell source code formatter (build from source)
+  language: golang
+  # Note: keep Go version in `go.mod` in sync with shfmt's required Go version
+  additional_dependencies: [mvdan.cc/sh/v3/cmd/shfmt@${shfmt_tag}]
+  entry: shfmt
+  args: [-w, -s]
+  types: [shell]
+  exclude_types: [csh, tcsh, zsh]
+  stages: [commit, merge-commit, push, manual]
+
+- id: shfmt-docker
+  name: shfmt
+  description: Shell source code formatter (Docker image)
+  language: docker_image
+  # Note: use the top level multiplatform image digest here
+  entry: --net none mvdan/shfmt:${shfmt_tag}@${docker_image_digest}
+  args: [-w, -s]
+  types: [shell]
+  exclude_types: [csh, tcsh, zsh]
+  stages: [commit, merge-commit, push, manual]
+"""
+
+
+def main(python_pkg_tag: str) -> None:
+    shfmt_tag, _, _ = python_pkg_tag.partition("-")
+
+    with contextlib.closing(docker.from_env()) as client:
+        registry_data = client.images.get_registry_data(f"mvdan/shfmt:{shfmt_tag}")
+
+    # Top level multiplatform image digest
+    docker_image_digest = registry_data.attrs["Descriptor"]["digest"]
+
+    data = {
+        "shfmt_tag": shfmt_tag,
+        "docker_image_digest": docker_image_digest,
+    }
+    st = string.Template(hooks_template.lstrip())
+    hooks_yaml = st.substitute(data)
+    sys.stdout.write(hooks_yaml)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(f"usage: {sys.argv[0]} PYTHON-PKG-TAG", file=sys.stderr)
+        sys.exit(2)
+    main(sys.argv[1])
---
+++ tools/generate_setup_cfg.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python3
+
+# Copyright 2023 Ville Skyttä
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# At least Python 3.11 is required.
+
+import hashlib
+import re
+import string
+import sys
+from urllib.parse import quote as urlquote, urljoin
+from urllib.request import urlopen
+
+setup_cfg_template = """
+[metadata]
+name = shfmt_py
+version = ${python_pkg_version}
+description = Shell source code formatter
+url = https://github.com/scop/pre-commit-shfmt
+# license here is for shfmt proper; Python packaging related files may have others, see their contents and classifiers below
+license = BSD 3-Clause
+classifiers =
+    Intended Audience :: Developers
+    License :: OSI Approved :: Apache Software License
+    License :: OSI Approved :: BSD License
+    License :: OSI Approved :: MIT License
+    Operating System :: MacOS
+    Operating System :: Microsoft :: Windows
+    Operating System :: POSIX :: Linux
+    Programming Language :: Go
+project_urls =
+    Upstream = https://github.com/mvdan/sh#shfmt
+
+[options]
+packages =
+python_requires = >=3.8
+setup_requires =
+    setuptools-download
+
+[setuptools_download]
+download_scripts =
+    [shfmt]
+    group = shfmt-binary
+    marker = sys_platform == "darwin" and platform_machine == "x86_64"
+    url = ${url_darwin_amd64}
+    sha256 = ${hexdigest_darwin_amd64}
+    [shfmt]
+    group = shfmt-binary
+    marker = sys_platform == "darwin" and platform_machine == "arm64"
+    url = ${url_darwin_arm64}
+    sha256 = ${hexdigest_darwin_arm64}
+    [shfmt]
+    group = shfmt-binary
+    # TODO: verify i386
+    marker = sys_platform == "linux" and platform_machine == "i386"
+    marker = sys_platform == "linux" and platform_machine == "i686"
+    url = ${url_linux_386}
+    sha256 = ${hexdigest_linux_386}
+    [shfmt]
+    group = shfmt-binary
+    marker = sys_platform == "linux" and platform_machine == "x86_64"
+    url = ${url_linux_amd64}
+    sha256 = ${hexdigest_linux_amd64}
+    [shfmt]
+    group = shfmt-binary
+    # TODO: verify armv6hf
+    marker = sys_platform == "linux" and platform_machine == "armv6hf"
+    marker = sys_platform == "linux" and platform_machine == "armv7l"
+    url = ${url_linux_arm}
+    sha256 = ${hexdigest_linux_arm}
+    [shfmt]
+    group = shfmt-binary
+    marker = sys_platform == "linux" and platform_machine == "aarch64"
+    url = ${url_linux_arm64}
+    sha256 = ${hexdigest_linux_arm64}
+    [shfmt.exe]
+    group = shfmt-binary
+    # TODO: verify both
+    marker = sys_platform == "win32" and platform_machine == "x86"
+    marker = sys_platform == "cygwin" and platform_machine == "i386"
+    url = ${url_windows_386}
+    sha256 = ${hexdigest_windows_386}
+    [shfmt.exe]
+    group = shfmt-binary
+    marker = sys_platform == "win32" and platform_machine == "AMD64"
+    marker = sys_platform == "cygwin" and platform_machine == "x86_64"
+    url = ${url_windows_amd64}
+    sha256 = ${hexdigest_windows_amd64}
+"""
+
+release_files = [
+    "shfmt_{main_tag}_darwin_amd64",
+    "shfmt_{main_tag}_darwin_arm64",
+    "shfmt_{main_tag}_linux_386",
+    "shfmt_{main_tag}_linux_amd64",
+    "shfmt_{main_tag}_linux_arm",
+    "shfmt_{main_tag}_linux_arm64",
+    "shfmt_{main_tag}_windows_386.exe",
+    "shfmt_{main_tag}_windows_amd64.exe",
+]
+
+
+def main(python_pkg_tag: str) -> None:
+    main_tag, _, _ = python_pkg_tag.partition("-")
+    base_url = f"https://github.com/mvdan/sh/releases/download/{urlquote(main_tag)}/"
+
+    data = {
+        "python_pkg_version": python_pkg_tag.lstrip("v"),
+    }
+    for fn in release_files:
+        if m := re.search(r"_([a-z0-9]+_[a-z0-9]+)(?:\.exe)?$", fn):
+            os_arch = m.group(1)
+        else:
+            raise ValueError(f"could not determine OS/arch from {fn}")
+        url = urljoin(base_url, urlquote(fn.format(main_tag=main_tag)))
+        with urlopen(url) as f:
+            hexdigest = hashlib.file_digest(f, "sha256").hexdigest()
+        data[f"url_{os_arch}"] = url
+        data[f"hexdigest_{os_arch}"] = hexdigest
+
+    st = string.Template(setup_cfg_template.lstrip())
+    setup_cfg = st.substitute(data)
+    sys.stdout.write(setup_cfg)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(f"usage: {sys.argv[0]} PYTHON-PKG-TAG", file=sys.stderr)
+        sys.exit(2)
+    main(sys.argv[1])
