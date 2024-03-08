129ba5421e44e5a724053e72721e31af26a036b6
Michael Saki <michael.saki@associates.cisa.dhs.gov>
2024-03-08 14:48:15 -0600

    Remove unused files

--- .github/workflows/test-bad.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -eu
-
-# This script is fed to the hooks defined in this repo in CI for a
-# basic smoke test. shfmt is expected to make changes to it.
-
-if true; then
-echo "Hello, test."
-fi
--- .github/workflows/test-good.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-
-# This script is fed to the hooks defined in this repo in CI for a
-# basic smoke test. It must not provoke any formatting changes or
-# errors from shfmt.
-
-echo "Hello, test."
