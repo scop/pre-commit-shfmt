5567d27446fdf50d3dab6639a4b460a3f3c4d0c6
Michael Saki <michael.saki@associates.cisa.dhs.gov>
2024-03-08 13:18:17 -0600

    Refactor configs in pre-commit-hooks for Hadolint
    The original version of generate_pre_commit_hooks.py was being used to
    auto generate the pre-commit-hooks.yaml. The shfmt configs have been
    stripped and changed for a basic hadolint execution.

--- tools/generate_pre_commit_hooks.py
+++ tools/generate_pre_commit_hooks.py
@@ -1,68 +1,23 @@
 #!/usr/bin/env python3
 
-import contextlib
 import string
 import sys
 
-import docker
-
-
 hooks_template = """
-- id: shfmt
-  name: shfmt
-  description: Shell source code formatter (prebuilt upstream executable)
+- id: hadolint
+  name: hadolint
+  description: Dockerfile linter
   language: python
-  entry: shfmt
-  args: [-w, -s]
-  types: [shell]
-  exclude_types: [csh, tcsh, zsh]
-  stages: [commit, merge-commit, push, manual]
-
-- id: shfmt-src
-  name: shfmt
-  description: Shell source code formatter (build from source)
-  language: golang
-  # Note: keep Go version in `go.mod` in sync with shfmt's required Go version
-  additional_dependencies: [mvdan.cc/sh/v3/cmd/shfmt@${shfmt_tag}]
-  entry: shfmt
-  args: [-w, -s]
-  types: [shell]
-  exclude_types: [csh, tcsh, zsh]
-  stages: [commit, merge-commit, push, manual]
-
-- id: shfmt-docker
-  name: shfmt
-  description: Shell source code formatter (Docker image)
-  language: docker_image
-  # Note: use the top level multiplatform image digest here
-  entry: --net none mvdan/shfmt:${shfmt_tag}@${docker_image_digest}
-  args: [-w, -s]
-  types: [shell]
-  exclude_types: [csh, tcsh, zsh]
+  entry: hadolint
+  types: [dockerfile]
   stages: [commit, merge-commit, push, manual]
 """
 
 
-def main(python_pkg_tag: str) -> None:
-    shfmt_tag, _, _ = python_pkg_tag.partition("-")
-
-    with contextlib.closing(docker.from_env()) as client:
-        registry_data = client.images.get_registry_data(f"mvdan/shfmt:{shfmt_tag}")
-
-    # Top level multiplatform image digest
-    docker_image_digest = registry_data.attrs["Descriptor"]["digest"]
-
-    data = {
-        "shfmt_tag": shfmt_tag,
-        "docker_image_digest": docker_image_digest,
-    }
-    st = string.Template(hooks_template.lstrip())
-    hooks_yaml = st.substitute(data)
+def main() -> None:
+    # If you later decide to use a dynamic value, like Hadolint version, you can reintroduce and use a data dictionary here.
+    hooks_yaml = hooks_template.strip()  # Use strip() if you want to remove leading/trailing whitespace for cleanliness.
     sys.stdout.write(hooks_yaml)
 
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print(f"usage: {sys.argv[0]} PYTHON-PKG-TAG", file=sys.stderr)
-        sys.exit(2)
-    main(sys.argv[1])
+    main()
